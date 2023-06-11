240281fc71a4dce078c3ca6f047a7bd34a94e598
krapli441 <tlfqj1208@gmail.com>
2023-06-11 16:28:47 +0900

    설명 추가

--- src/views/useEffect.tsx
+++ src/views/useEffect.tsx
@@ -15,6 +15,10 @@ export default function effect() {
     <div>
       <p>Count : {count}</p>
       <button onClick={() => setCount(count + 1)}>증가시키기</button>
+      <p>
+        위 버튼을 누를 때마다 개발자 도구에서 useEffect가 작동하는 것을 확인할
+        수 있습니다.
+      </p>
     </div>
   );
 }
