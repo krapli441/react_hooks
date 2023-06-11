824da9a4b8576e20906abe45870918a3fc323777
krapli441 <tlfqj1208@gmail.com>
2023-06-11 15:40:42 +0900

    App.tsx의 문구 통일

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,7 +4,7 @@ import Effect from "./useEffect";
 export default function App() {
   return (
     <>
-      <h1>테스트 해보겠습니다.</h1>
+      <h1>리액트의 훅스를 알아보겠습니다</h1>
       <Effect></Effect>
     </>
   );
