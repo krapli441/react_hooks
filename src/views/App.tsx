8efca305cb01d344b2550c058d22dc161441470a
krapli441 <tlfqj1208@gmail.com>
2023-06-11 16:14:27 +0900

    연습한 훅들을 모아왔다.

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import State from "./UseState";
 import Modal from "./UseStateModal";
-import { ParentComponent,ChildComponent } from "./useContext";
+import { ParentComponent, ChildComponent } from "./useContext";
+import Effect from "./useEffect";
 
 export default function App() {
   return (
@@ -9,8 +10,9 @@ export default function App() {
       <h1>리액트의 훅스를 알아보겠습니다</h1>
       <State></State>
       <Modal></Modal>
+      <Effect />
       <ParentComponent />
-      <ChildComponent/>
+      <ChildComponent />
     </>
   );
 }
