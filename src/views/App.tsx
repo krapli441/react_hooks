57fa4b9ea3e053ba63af9a14d27a00255cad472d
krapli441 <tlfqj1208@gmail.com>
2023-06-11 15:40:02 +0900

    useEffect 훅을 이용해 컴포넌트가 마운트되거나 언마운트될 때 실행된다는 것을 확인.

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Effect from "./useEffect";
 
 export default function App() {
   return (
     <>
       <h1>테스트 해보겠습니다.</h1>
+      <Effect></Effect>
     </>
   );
 }
